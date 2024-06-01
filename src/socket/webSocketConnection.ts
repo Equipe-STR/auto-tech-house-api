f628d4ad0b505f388b21b0dedbc86fbe8622300f
GabrielT23 <gabaraujoteixeira@gmail.com>
2024-06-01 10:27:55 -0300

    feat:configuring_send_email

--- src/socket/webSocketConnection.ts
+++ src/socket/webSocketConnection.ts
@@ -1,5 +1,6 @@
 import { wss } from '../app';
 import 'express-async-errors';
+import { sendEmail } from '../utils/sendEmail';
 import WebSocket, { WebSocketServer, WebSocket as wsType } from 'ws';
 
 wss.on('connection', (ws: wsType) => {
@@ -7,6 +8,9 @@ wss.on('connection', (ws: wsType) => {
 
   ws.on('message', (message: string) => {
       console.log(message.toString());
+      sendEmail(process.env.MAIL_DEST, 'Disparo de sensor', 'sua casa foi invadida', '<b>sua casa foi invadida</b>')
+      .then(messageId => console.log(`E-mail enviado com sucesso, ID: ${messageId}`))
+      .catch(error => console.error(`Erro ao enviar e-mail: ${error}`));
       wss.clients.forEach((client) => {
           if (client !== ws && client.readyState === WebSocket.OPEN) {
               client.send(message.toString());
---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,32 @@
+import nodemailer from 'nodemailer';
+
+export async function sendEmail(to: string, subject: string, text:string, html:string) {
+    let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST ?? 'smtp.gmail.com',
+        port: Number(process.env.MAIL_PORT) ?? 587,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_USER ?? 'seu-email@example.com',
+            pass: process.env.MAIL_PASS ?? 'sua-senha'
+        }
+    });
+
+    let mailOptions = {
+        from: process.env.MAIL_NAME + " "+ process.env.MAIL_USER,
+        to: to,
+        subject: subject,
+        text: text,
+        html: html
+    };
+
+    try {
+        // Envio do e-mail
+        let info = await transporter.sendMail(mailOptions);
+        console.log('E-mail enviado: %s', info.messageId);
+        return info.messageId;
+    } catch (error) {
+        console.error('Erro ao enviar e-mail: %s', error);
+        throw error;
+    }
+}
+
